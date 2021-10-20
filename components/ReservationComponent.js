4895841a984e6e46ade1a83bd0843f6782e76126
John Dellenbaugh <johndell@microsoft.com>
2021-10-19 18:55:25 -0700

    local notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,7 +3,7 @@ import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } fro
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { createAnimatableComponent } from 'react-native-animatable';
 import * as Animatable from 'react-native-animatable';
-// import Picker from '@react-native-community/picker';
+import * as Notifications from 'expo-notifications';
 
 console.disableYellowBox=true
 
@@ -38,9 +38,12 @@ class Reservation extends Component {
                 onPress:() => {this.resetForm()},
             },
             {
-                text: "OK",
-                onPress:() => {this.resetForm()},
-            },
+                text: 'OK', 
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    }
+            }
         ]
         
         );
@@ -56,8 +59,34 @@ class Reservation extends Component {
         });
     }
 
+  async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
+
     render() {
-        return (
+        return (    
             <ScrollView>
                 <Animatable.View animation='zoomIn' duration= {2000} delay= {1000}>
                     <View style={styles.formRow}>
